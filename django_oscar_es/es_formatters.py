32b06741a3bb59080fcc9cb9c3c45b6186e439c6
Joey Jurjens <joeyjurjens@gmail.com>
2024-06-07 23:23:45 +0200

    Make the default formatters error proof, eg for ints etc

--- django_oscar_es/es_formatters.py
+++ django_oscar_es/es_formatters.py
@@ -3,9 +3,17 @@ from .formatters.registry import register_formatter
 
 @register_formatter("uppercase_formatter")
 def uppercase_formatter(request, key, doc_count):
+    try:
+        key = key.upper()
+    except Exception:
+        key = key
     return f"{key.upper()} ({doc_count})"
 
 
 @register_formatter("lowercase_formatter")
 def lowercase_formatter(request, key, doc_count):
+    try:
+        key = key.lower()
+    except Exception:
+        key = key
     return f"{key.lower()} ({doc_count})"
